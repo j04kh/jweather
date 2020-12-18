019a70a8f5fd964c0e01dfff8a9886f69ff84063
Joaquin <joaquin.hernandez@fing.edu.uy>
2020-12-18 03:07:35 -0300

    added API call for weather info

--- src/App.js
+++ src/App.js
@@ -1,14 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import './App.css';
 import WeatherData from './components/WeatherData';
 import NavBar from './components/NavBar';
 
 function App() {
+
+  let coordLat = -34;
+  let coordLong = 56;
+
+  let temp = 0;
+  let city = '';
+  let country = '';
+  let wind = 0;
+  let rain = 0;
+  let pressure = 0;
+  let humidity = 0;
+  let img = '';
+
+
+  const [coords, updateCoords] = useState([coordLat, coordLong]);
+
+  const [weatherValues, updateWeatherValues] = useState(
+    temp = 0,
+    city = '',
+    country = '',
+    wind = 0,
+    rain = 0,
+    pressure = 0,
+    humidity = 0,
+    img = ''
+  )
+
+  useEffect(() => getLocationInfo(), []);
+  
+  
+  const getLocationInfo = () => {
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) => {
+        coordLat = position.coords.latitude;
+        coordLong = position.coords.longitude;
+
+        let newCoords = [coordLat, coordLong];
+        updateCoords(newCoords);
+       
+        console.log("Coordenadas: " + coordLat + coordLong);
+
+        Axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${coordLat}&lon=${coordLong}&units=metric&appid=9a1a834849335352ddd99f2f95f6ae9d`).then(
+        res => {
+          console.log(res);
+          let newWeatherValues = [
+            temp = res.data.main.temp,
+            city = res.data.name,
+            country = res.data.sys.country,
+            wind = res.data.wind.speed,
+            rain = 0,
+            pressure = res.data.main.pressure,
+            humidity = res.data.main.humidity,
+            img = res.data.weather.icon
+          ]
+          updateWeatherValues(newWeatherValues);
+        }
+      )
+      })
+
+    } else {
+      console.log("Geolocation is not supported.");
+    }
+  }
+
+  
+
   return (
     <div className="App">
       <NavBar />
       <div className="content-wrapper">
-        <WeatherData />
+        <WeatherData weatherValues={weatherValues} />
       </div>
     </div>
   );
--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,32 +1,40 @@
 import React from 'react';
 
-const WeatherData = () => {
+const WeatherData = (props) => {
+
+    console.log("PROPS: " + props.weatherValues);
+    
+    // I'm Using the logic operator '?' to show a '-' when de data is not loaded, instead of showing 'NaN' or something like that
+
     return(
         <div className="main-container">
             <div className="container-up">
-
                 <div className="col-left">
-                    <div className="temp"> 25ºC</div>
-                    <div className="location">Flores, UY</div>
+                    <div className="temp"> 
+                      {props.weatherValues[0]==undefined ? "-" : Math.round(props.weatherValues[0])+"ºC"}
+                    </div>
+                    <div className="location">
+                        {props.weatherValues[1]==undefined ? "-" : props.weatherValues[1]}, {props.weatherValues[2]==undefined ? "-" : props.weatherValues[2]}
+                    </div>
                 </div>
 
                 <div className="weather-img"></div>
             </div>
             <div className="container-bottom">
                 <div className="data-item">
-                    <p>20 km/h</p>
+                    <p>{props.weatherValues[3]==undefined ? "-" : Math.round(props.weatherValues[3])+"km/h"} </p>
                     <h1>Wind</h1>
                 </div>
                 <div className="data-item">
-                    <p>0 %</p>
+                    <p>{props.weatherValues[4]==undefined ? "-" : props.weatherValues[4]+"%"} </p>
                     <h1>Rain</h1>
                 </div>
                 <div className="data-item">
-                    <p> 1013 hpa</p>
+                    <p> {props.weatherValues[5]==undefined ? "-" : props.weatherValues[5]+"hpa"}</p>
                     <h1>Pressure</h1>
                 </div>
                 <div className="data-item">
-                    <p>42 %</p>
+                    <p>{props.weatherValues[6]==undefined ? "-" : props.weatherValues[6]+"%"}</p>
                     <h1>Humidity</h1>
                 </div>
             </div>
