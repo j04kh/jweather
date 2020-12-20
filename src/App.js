173897b3c94cacacac83272eec6364f75d57e8e6
Joaquin <joaquin.hernandez@fing.edu.uy>
2020-12-19 21:47:06 -0300

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import Axios from 'axios';
 import './App.css';
 import WeatherData from './components/WeatherData';
 import NavBar from './components/NavBar';
+import Search from "./assets/search-icon.png";
 
 function App() {
-
-  let coordLat = -34;
-  let coordLong = 56;
 
   let temp = 0;
   let city = '';
@@ -18,8 +16,6 @@ function App() {
   let humidity = 0;
   let img = '';
   let desc = '';
-
-  const [coords, updateCoords] = useState([coordLat, coordLong]);
 
   const [weatherValues, updateWeatherValues] = useState(
     temp = 0,
@@ -33,45 +29,32 @@ function App() {
     desc = ''
   );
 
-  const [searchQuery, updateSearchQuery] = useState('');
+  const [searchQuery, updateSearchQuery] = useState('montevideo');
 
-  useEffect(() => getLocationInfo(), []);
+  useEffect(() => getInfo(), []);
   
-  
-  const getLocationInfo = () => {
-    if (navigator.geolocation){
-      navigator.geolocation.getCurrentPosition((position) => {
-        coordLat = position.coords.latitude;
-        coordLong = position.coords.longitude;
+  const getInfo = () => {
 
-        let newCoords = [coordLat, coordLong];
-        updateCoords(newCoords);
-       
-        console.log("Coordenadas: " + coordLat + coordLong);
-
-        Axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${coordLat}&lon=${coordLong}&units=metric&appid=9a1a834849335352ddd99f2f95f6ae9d`).then(
-        res => {
-          console.log(res);
-          let newWeatherValues = [
-            temp = res.data.main.temp,
-            city = res.data.name,
-            country = res.data.sys.country,
-            wind = res.data.wind.speed,
-            rain = res.data.rain,
-            pressure = res.data.main.pressure,
-            humidity = res.data.main.humidity,
-            img = res.data.weather[0].icon,
-            desc = res.data.weather[0].description
-          ]
-          updateWeatherValues(newWeatherValues);
-        }
-      )
-      })
-
-    } else {
-      console.log("Geolocation is not supported.");
+    Axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&units=metric&appid=9a1a834849335352ddd99f2f95f6ae9d`).then(
+      res => {
+        console.log(res);
+        let newWeatherValues = [
+          temp = res.data.main.temp,
+          city = res.data.name,
+          country = res.data.sys.country,
+          wind = res.data.wind.speed,
+          rain = res.data.rain,
+          pressure = res.data.main.pressure,
+          humidity = res.data.main.humidity,
+          img = res.data.weather[0].icon,
+          desc = res.data.weather[0].description
+        ]
+        updateWeatherValues(newWeatherValues);
+        updateSearchQuery('');
+      }
+    )
     }
-  }
+  
 
   const searchboxHandler = (e) => {
     console.log("BUSCA? " + searchQuery);
@@ -103,6 +86,7 @@ function App() {
         <WeatherData weatherValues={weatherValues} />
         <form onSubmit={e => searchboxHandler(e)} onChange={e => updateSearchQuery(e.target.value)}>
           <input type="text" name="search" className="searchbox" value={searchQuery} placeholder="Find your city"></input>
+          <button type="submit" className="btn"><img id="search-icon" src={Search}></img></button>
         </form>
       </div>
     </div>
