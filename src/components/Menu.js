ede258a36089eed4b5444591fd08e6b0f7a9be0a
Joaquin <joaquin.hernandez@fing.edu.uy>
2021-01-23 16:18:36 -0300

    added menu

---
+++ src/components/Menu.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'; 
+
+import './Menu.css';
+
+const Menu = (props) => {
+
+    const [unit, setUnit] = useState('metric');
+
+    const toggleUnits = () =>{
+        props.toggleU();
+    }
+
+    const changeUnits = (e) => {
+        toggleUnits();
+        unit === 'metric' ? setUnit('imperial') : setUnit('metric');
+    }
+    
+    return(
+        <div className={`menu-${props.st}`}>
+            <p className={`menu-item${props.units === 'metric' ? '-active' : ''}`} onClick={e => changeUnits(e)}>ºC</p> 
+            <p className={`menu-item${props.units === 'imperial' ? '-active' : ''}`} onClick={e => changeUnits(e)}>ºF</p>
+        </div>
+    );
+};
+
+export default Menu;
